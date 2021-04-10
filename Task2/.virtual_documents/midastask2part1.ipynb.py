044977f873810b13dd03ef6fb262f0ae3a3a6a48
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2021-04-10 23:20:59 +0530

    Part 1 over

--- Task2/.virtual_documents/midastask2part1.ipynb.py
+++ Task2/.virtual_documents/midastask2part1.ipynb.py
@@ -27,11 +27,11 @@ print(image.mode)
 imshow(image)
 
 
-train_datagen1 = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+train_datagen1 = ImageDataGenerator(rescale=1./255, validation_split=0.2, preprocessing_function=lambda x: 1-x)
 
 
 BATCH_SIZE = 64
-IMAGE_SIZE = (45, 60)
+IMAGE_SIZE = (21, 28)
 EPOCHS = 400
 
 
@@ -77,10 +77,22 @@ for i in range(1, 17):
 plt.show() 
 
 
+early_stopping_callback = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    mode='min',
+    patience=20,
+    restore_best_weights=True,
+    verbose=1)
+
+
 model1 = Sequential()
 
+# Lambda Layer for adding Padding
+model1.add(Lambda(lambda image: tf.image.resize_with_crop_or_pad(
+        image, 28, 28), input_shape=(*IMAGE_SIZE, 1)))
+
 # 1st Convolution Layer
-model1.add(Conv2D(6, input_shape=(*IMAGE_SIZE, 1), kernel_size=(5,5), padding='same', activation='relu'))
+model1.add(Conv2D(6, kernel_size=(5,5), padding='same', activation='relu'))
 model1.add(BatchNormalization())
 model1.add(MaxPooling2D(pool_size=(2,2), strides=2))
 
@@ -110,14 +122,6 @@ model1.summary()
 
 
 model1.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-
-
-early_stopping_callback = tf.keras.callbacks.EarlyStopping(
-    monitor='val_loss',
-    mode='min',
-    patience=20,
-    restore_best_weights=True,
-    verbose=1)
 
 
 checkpoint_filepath1 = 'exp1/checkpoint'
@@ -157,7 +161,7 @@ plt.legend(['training', 'validation'], loc='best')
 plt.show()
 
 
-no_augmentation = ImageDataGenerator(rescale=1./255)
+no_augmentation = ImageDataGenerator(rescale=1./255, preprocessing_function=lambda x: 1-x)
 
 no_augmentation_gen = no_augmentation.flow_from_directory(
         '../input/trainpart1zip/train',
@@ -176,7 +180,7 @@ for i in range(1, 17):
 plt.show() 
 
 
-augmentation_test_rotation = ImageDataGenerator(rescale=1./255, rotation_range=15)
+augmentation_test_rotation = ImageDataGenerator(rescale=1./255, rotation_range=15, preprocessing_function=lambda x: 1-x)
 
 augmentation_test_rotation_gen = augmentation_test_rotation.flow_from_directory(
         '../input/trainpart1zip/train',
@@ -195,7 +199,7 @@ for i in range(1, 17):
 plt.show() 
 
 
-augmentation_test_shear = ImageDataGenerator(rescale=1./255, shear_range=0.3)
+augmentation_test_shear = ImageDataGenerator(rescale=1./255, shear_range=0.3, preprocessing_function=lambda x: 1-x)
 
 augmentation_test_shear_gen = augmentation_test_shear.flow_from_directory(
         '../input/trainpart1zip/train',
@@ -214,7 +218,7 @@ for i in range(1, 17):
 plt.show() 
 
 
-augmentation_test_zoom = ImageDataGenerator(rescale=1./255, zoom_range=0.2)
+augmentation_test_zoom = ImageDataGenerator(rescale=1./255, zoom_range=0.2, preprocessing_function=lambda x: 1-x)
 
 augmentation_test_zoom_gen = augmentation_test_zoom.flow_from_directory(
         '../input/trainpart1zip/train',
@@ -233,7 +237,7 @@ for i in range(1, 17):
 plt.show() 
 
 
-augmentation_test_shift = ImageDataGenerator(rescale=1./255, width_shift_range=0.2, height_shift_range=0.2)
+augmentation_test_shift = ImageDataGenerator(rescale=1./255, width_shift_range=0.2, height_shift_range=0.2, preprocessing_function=lambda x: 1-x)
 
 augmentation_test_shift_gen = augmentation_test_shift.flow_from_directory(
         '../input/trainpart1zip/train',
@@ -256,11 +260,13 @@ train_datagen2 = ImageDataGenerator(
     rescale=1./255,
     validation_split=0.2,
     shear_range=0.1,
-    rotation_range=0.5)
+    rotation_range=0.5,
+    preprocessing_function=lambda x: 1-x)
 
 validation_datagen2 = ImageDataGenerator(
     rescale=1./255,
-    validation_split=0.2)
+    validation_split=0.2,
+    preprocessing_function=lambda x: 1-x)
 
 
 train_generator2 = train_datagen2.flow_from_directory(
@@ -305,8 +311,12 @@ plt.show()
 
 model2 = Sequential()
 
+# Lambda Layer for adding Padding
+model2.add(Lambda(lambda image: tf.image.resize_with_crop_or_pad(
+        image, 28, 28), input_shape=(*IMAGE_SIZE, 1)))
+
 # 1st Convolution Layer
-model2.add(Conv2D(6, input_shape=(*IMAGE_SIZE, 1), kernel_size=(5,5), padding='same', activation='relu'))
+model2.add(Conv2D(6, kernel_size=(5,5), padding='same', activation='relu'))
 model2.add(BatchNormalization())
 model2.add(MaxPooling2D(pool_size=(2,2), strides=2))
 
@@ -382,9 +392,12 @@ def mish(x):
 
 model3 = Sequential()
 
+# Lambda Layer for adding Padding
+model3.add(Lambda(lambda image: tf.image.resize_with_crop_or_pad(
+        image, 28, 28), input_shape=(*IMAGE_SIZE, 1)))
+
 # 1st Convolution Layer
-model3.add(Conv2D(6, input_shape=(*IMAGE_SIZE, 1),
-                  kernel_size=(5,5), padding='same', activation=mish))
+model3.add(Conv2D(6, kernel_size=(5,5), padding='same', activation='relu'))
 model3.add(BatchNormalization())
 model3.add(MaxPooling2D(pool_size=(2,2), strides=2))
 
@@ -453,111 +466,39 @@ plt.legend(['training', 'validation'], loc='best')
 plt.show()
 
 
-model7 = Sequential()
+model4 = Sequential()
+
+# Lambda Layer for adding Padding
+model4.add(Lambda(lambda image: tf.image.resize_with_crop_or_pad(
+        image, 28, 28), input_shape=(*IMAGE_SIZE, 1)))
 
 # 1st Convolution Layer
-model7.add(Conv2D(6, input_shape=(*IMAGE_SIZE, 1),
-                  kernel_size=(5,5), padding='same', activation=mish))
-model7.add(BatchNormalization())
-model7.add(MaxPooling2D(pool_size=(2,2), strides=2))
+model4.add(Conv2D(6, kernel_size=(5,5), padding='same', activation='relu'))
+model4.add(BatchNormalization())
+model4.add(MaxPooling2D(pool_size=(2,2), strides=2))
 
 # 2nd Convolution Layer
-model7.add(Conv2D(16, kernel_size=(5,5), activation=mish))
-model7.add(BatchNormalization())
-model7.add(MaxPooling2D(pool_size=(2,2), strides=2))
+model4.add(Conv2D(16, kernel_size=(5,5), activation=mish))
+model4.add(BatchNormalization())
+model4.add(MaxPooling2D(pool_size=(2,2), strides=2))
 
 # Passing to a Fully Connected Layer
-model7.add(Flatten())
+model4.add(Flatten())
 
 # 1st Fully Connected Layer
-model7.add(Dense(256, activation=mish))
-model7.add(BatchNormalization())
-model7.add(Dropout(0.4))
+model4.add(Dense(256, activation=mish))
+model4.add(BatchNormalization())
+model4.add(Dropout(0.4))
 
 # 2nd Fully Connected Layer
-model7.add(Dense(128, activation=mish))
-model7.add(BatchNormalization())
-model7.add(Dropout(0.4))
+model4.add(Dense(128, activation=mish))
+model4.add(BatchNormalization())
+model4.add(Dropout(0.4))
 
 # Output Layer
 # Increasing the softmax temperature
 temp = 5
-model7.add(Lambda(lambda x: x / temp))
-model7.add(Dense(62, activation='softmax'))
-
-
-model7.summary()
-
-
-model7.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-
-
-checkpoint_filepath7 = 'exp7/checkpoint'
-model_checkpoint_callback7 = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath7,
-    save_weights_only=True,
-    monitor='val_loss',
-    mode='min',
-    save_best_only=True)
-
-
-history7 = model7.fit(
-    train_generator1,
-    epochs=EPOCHS,
-    validation_data=validation_generator1,
-    steps_per_epoch = train_generator1.samples // BATCH_SIZE,
-    validation_steps = validation_generator1.samples // BATCH_SIZE,
-    callbacks=[model_checkpoint_callback7, early_stopping_callback]
-)
-
-
-plt.plot(history7.history['accuracy'])
-plt.plot(history7.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['training', 'validation'], loc='best')
-plt.show()
-
-
-plt.plot(history7.history['loss'])
-plt.plot(history7.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['training', 'validation'], loc='best')
-plt.show()
-
-
-model4 = Sequential()
-
-# 1st Convolution Layer
-model4.add(Conv2D(32, input_shape=(*IMAGE_SIZE, 1), kernel_size=3, activation=mish))
-model4.add(BatchNormalization())
-model4.add(Conv2D(32, kernel_size=3, activation=mish))
-model4.add(BatchNormalization())
-model4.add(Conv2D(32, kernel_size=5, strides=2, padding='same', activation=mish))
-model4.add(BatchNormalization())
-model4.add(Dropout(0.4))
-
-# 2nd Convolution Layer
-model4.add(Conv2D(64, kernel_size=3, activation=mish))
-model4.add(BatchNormalization())
-model4.add(Conv2D(64, kernel_size=3, activation=mish))
-model4.add(BatchNormalization())
-model4.add(Conv2D(64, kernel_size=5, strides=2, padding='same', activation=mish))
-model4.add(BatchNormalization())
-model4.add(Dropout(0.4))
-
-# 3rd Convolution Layer
-model4.add(Conv2D(128, kernel_size = 4, activation=mish))
-model4.add(BatchNormalization())
-
-# Passing to a Fully Connected Layer
-model4.add(Flatten())
-model4.add(Dropout(0.4))
-
-# Output Layer
+model4.add(Lambda(lambda x: x / temp))
 model4.add(Dense(62, activation='softmax'))
 
 
@@ -604,9 +545,88 @@ plt.legend(['training', 'validation'], loc='best')
 plt.show()
 
 
+model5 = Sequential()
+
+# Lambda Layer for adding Padding
+model5.add(Lambda(lambda image: tf.image.resize_with_crop_or_pad(
+        image, 28, 28), input_shape=(*IMAGE_SIZE, 1)))
+
+# 1st Convolution Layer
+model5.add(Conv2D(32, kernel_size=3, activation=mish))
+model5.add(BatchNormalization())
+model5.add(Conv2D(32, kernel_size=3, activation=mish))
+model5.add(BatchNormalization())
+model5.add(Conv2D(32, kernel_size=5, strides=2, padding='same', activation=mish))
+model5.add(BatchNormalization())
+model5.add(Dropout(0.4))
+
+# 2nd Convolution Layer
+model5.add(Conv2D(64, kernel_size=3, activation=mish))
+model5.add(BatchNormalization())
+model5.add(Conv2D(64, kernel_size=3, activation=mish))
+model5.add(BatchNormalization())
+model5.add(Conv2D(64, kernel_size=5, strides=2, padding='same', activation=mish))
+model5.add(BatchNormalization())
+model5.add(Dropout(0.4))
+
+# 3rd Convolution Layer
+model5.add(Conv2D(128, kernel_size = 4, activation=mish))
+model5.add(BatchNormalization())
+
+# Passing to a Fully Connected Layer
+model5.add(Flatten())
+model5.add(Dropout(0.4))
+
+# Output Layer
+model5.add(Dense(62, activation='softmax'))
+
+
+model5.summary()
+
+
+model5.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+
+
 checkpoint_filepath5 = 'exp5/checkpoint'
 model_checkpoint_callback5 = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_filepath5,
+    save_weights_only=True,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+
+history5 = model5.fit(
+    train_generator1,
+    epochs=EPOCHS,
+    validation_data=validation_generator1,
+    steps_per_epoch = train_generator1.samples // BATCH_SIZE,
+    validation_steps = validation_generator1.samples // BATCH_SIZE,
+    callbacks=[model_checkpoint_callback5, early_stopping_callback]
+)
+
+
+plt.plot(history5.history['accuracy'])
+plt.plot(history5.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['training', 'validation'], loc='best')
+plt.show()
+
+
+plt.plot(history5.history['loss'])
+plt.plot(history5.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['training', 'validation'], loc='best')
+plt.show()
+
+
+checkpoint_filepath6 = 'exp6/checkpoint'
+model_checkpoint_callback6 = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath6,
     save_weights_only=True,
     monitor='val_loss',
     mode='min',
@@ -636,9 +656,13 @@ DENSE_KERNEL_INITIALIZER = {
 # Input Layer
 inputs = tf.keras.layers.Input(shape=(*IMAGE_SIZE, 1))
 
+# Lambda Layer for adding Padding
+x = tf.keras.layers.Lambda(lambda image: tf.image.resize_with_crop_or_pad(
+        image, 28, 28), input_shape=(*IMAGE_SIZE, 1))(inputs)
+
 # Efficient layer except the top layer
 x = EfficientNetB0(include_top=False, weights=None,
-    input_shape=(*IMAGE_SIZE, 1))(inputs)
+    input_shape=(48, 48, 1))(x)
 
 # Top
 
@@ -652,27 +676,27 @@ outputs = tf.keras.layers.Dense(62,
     kernel_initializer=DENSE_KERNEL_INITIALIZER,
     name='predictions')(x)
 
-model5 = tf.keras.Model(inputs, outputs)
+model6 = tf.keras.Model(inputs, outputs)
 
-model5.compile(
+model6.compile(
     optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
 )
 
-model5.summary()
+model6.summary()
 
 
-history5 = model5.fit(
+history6 = model6.fit(
     train_generator1,
     epochs=EPOCHS,
     validation_data=validation_generator1,
     steps_per_epoch = train_generator1.samples // BATCH_SIZE,
     validation_steps = validation_generator1.samples // BATCH_SIZE,
-    callbacks=[model_checkpoint_callback5, early_stopping_callback2]
+    callbacks=[model_checkpoint_callback6, early_stopping_callback2]
 )
 
 
-plt.plot(history5.history['accuracy'])
-plt.plot(history5.history['val_accuracy'])
+plt.plot(history6.history['accuracy'])
+plt.plot(history6.history['val_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
@@ -680,180 +704,8 @@ plt.legend(['training', 'validation'], loc='best')
 plt.show()
 
 
-plt.plot(history5.history['loss'])
-plt.plot(history5.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['training', 'validation'], loc='best')
-plt.show()
-
-
-BATCH_SIZE = 32
-
-
-train_generator3 = train_datagen1.flow_from_directory(
-        '../input/trainpart1zip/train',
-        target_size=IMAGE_SIZE,
-        batch_size=BATCH_SIZE,
-        class_mode='categorical',
-        color_mode='grayscale',
-        subset='training',
-        seed=42,
-        shuffle=True)
-
-validation_generator3 = train_datagen1.flow_from_directory(
-        '../input/trainpart1zip/train',
-        target_size=tf.squeeze(IMAGE_SIZE),
-        batch_size=BATCH_SIZE,
-        class_mode='categorical',
-        color_mode='grayscale',
-        subset='validation',
-        seed=42,
-        shuffle=True)
-
-
-model6_1 = Sequential()
-
-# 1st Convolution Layer
-model6_1.add(Conv2D(6, input_shape=(*IMAGE_SIZE, 1),
-                  kernel_size=(5,5), padding='same', activation=mish))
-model6_1.add(BatchNormalization())
-model6_1.add(MaxPooling2D(pool_size=(2,2), strides=2))
-
-# 2nd Convolution Layer
-model6_1.add(Conv2D(16, kernel_size=(5,5), activation=mish))
-model6_1.add(BatchNormalization())
-model6_1.add(MaxPooling2D(pool_size=(2,2), strides=2))
-
-# Passing to a Fully Connected Layer
-model6_1.add(Flatten())
-
-# 1st Fully Connected Layer
-model6_1.add(Dense(256, activation=mish))
-model6_1.add(BatchNormalization())
-model6_1.add(Dropout(0.4))
-
-# 2nd Fully Connected Layer
-model6_1.add(Dense(128, activation=mish))
-model6_1.add(BatchNormalization())
-model6_1.add(Dropout(0.4))
-
-# Output Layer
-model6_1.add(Dense(62, activation='softmax'))
-
-
-model6_1.summary()
-
-
-model6_1.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-
-
-checkpoint_filepath6_1 = 'exp6_1/checkpoint'
-model_checkpoint_callback6_1 = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath6_1,
-    save_weights_only=True,
-    monitor='val_loss',
-    mode='min',
-    save_best_only=True)
-
-
-history6_1 = model6_1.fit(
-    train_generator3,
-    epochs=EPOCHS,
-    validation_data=validation_generator3,
-    steps_per_epoch = train_generator3.samples // BATCH_SIZE,
-    validation_steps = validation_generator3.samples // BATCH_SIZE,
-    callbacks=[model_checkpoint_callback6_1, early_stopping_callback]
-)
-
-
-plt.plot(history6_1.history['accuracy'])
-plt.plot(history6_1.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['training', 'validation'], loc='best')
-plt.show()
-
-
-plt.plot(history6_1.history['loss'])
-plt.plot(history6_1.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['training', 'validation'], loc='best')
-plt.show()
-
-
-model6_2 = Sequential()
-
-# 1st Convolution Layer
-model6_2.add(Conv2D(32, input_shape=(*IMAGE_SIZE, 1), kernel_size=3, activation=mish))
-model6_2.add(BatchNormalization())
-model6_2.add(Conv2D(32, kernel_size=3, activation=mish))
-model6_2.add(BatchNormalization())
-model6_2.add(Conv2D(32, kernel_size=5, strides=2, padding='same', activation=mish))
-model6_2.add(BatchNormalization())
-model6_2.add(Dropout(0.4))
-
-# 2nd Convolution Layer
-model6_2.add(Conv2D(64, kernel_size=3, activation=mish))
-model6_2.add(BatchNormalization())
-model6_2.add(Conv2D(64, kernel_size=3, activation=mish))
-model6_2.add(BatchNormalization())
-model6_2.add(Conv2D(64, kernel_size=5, strides=2, padding='same', activation=mish))
-model6_2.add(BatchNormalization())
-model6_2.add(Dropout(0.4))
-
-# 3rd Convolution Layer
-model6_2.add(Conv2D(128, kernel_size = 4, activation=mish))
-model6_2.add(BatchNormalization())
-
-# Passing to a Fully Connected Layer
-model6_2.add(Flatten())
-model6_2.add(Dropout(0.4))
-
-# Output Layer
-model6_2.add(Dense(62, activation='softmax'))
-
-
-model6_2.summary()
-
-
-model6_2.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-
-
-checkpoint_filepath6_2 = 'exp6_2/checkpoint'
-model_checkpoint_callback6_2 = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath6_2,
-    save_weights_only=True,
-    monitor='val_loss',
-    mode='min',
-    save_best_only=True)
-
-
-history6_2 = model6_2.fit(
-    train_generator3,
-    epochs=EPOCHS,
-    validation_data=validation_generator3,
-    steps_per_epoch = train_generator3.samples // BATCH_SIZE,
-    validation_steps = validation_generator3.samples // BATCH_SIZE,
-    callbacks=[model_checkpoint_callback6_2, early_stopping_callback]
-)
-
-
-plt.plot(history6_2.history['accuracy'])
-plt.plot(history6_2.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['training', 'validation'], loc='best')
-plt.show()
-
-
-plt.plot(history6_2.history['loss'])
-plt.plot(history6_2.history['val_loss'])
+plt.plot(history6.history['loss'])
+plt.plot(history6.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
